b8748f2fca21284fd4b14443ecff86491f1529f1
Krzysztof Piesiewicz <krz.piesiewicz@gmail.com>
2019-04-11 18:29:17 +0200

    CardTest, ActionTest, CardsMap

--- game/src/main/scala/game/example/Cards.scala
+++ game/src/main/scala/game/example/Cards.scala
@@ -1,6 +1,7 @@
 package game.example
 
 import game.core._
+import scala.reflect.ClassTag
 
 final class AtomicButtonCard protected () extends TypedCard[AtomicButtonCard] with CardWithPriority {
   val priority = 1
@@ -31,3 +32,25 @@ final class PriorityIncrementerCard protected () extends TypedCard[PriorityIncre
 final class TrenningCard protected () extends TypedCard[TrenningCard]
 
 final class CatCard protected () extends TypedCard[CatCard]
+
+object Cards {
+
+  def nameOf(c: Card): String = cardsMap.tagsToNames.getOrElse(c.tag, "unknown_card")
+
+  def tagForName(name: String): Option[ClassTag[_ <: Card]] = cardsMap.namesToTags.get(name)
+
+  private val cardsMap = CardsMap().
+    withCard[AtomicButtonCard]("atomic_bomb").
+    withCard[AttackCard]("attack").
+    withCard[MassedAttackCard]("mass_attack").
+    withCard[ShelterCard]("shelter").
+    withCard[DefenceCard]("defence").
+    withCard[ReversalCard]("reflection").
+    withCard[PriorityIncrementerCard]("priority_inc").
+    withCard[TrenningCard]("trenning").
+    withCard[CatCard]("cat")
+
+  private case class CardsMap(val namesToTags: Map[String, ClassTag[_ <: Card]] = Map.empty, val tagsToNames: Map[ClassTag[_ <: Card], String] = Map.empty) {
+    def withCard[C <: Card](name: String)(implicit tag: ClassTag[C]) = CardsMap(namesToTags + (name -> tag), tagsToNames + (tag -> name))
+  }
+}
--- game/src/test/scala/ActionTest.scala
+++ game/src/test/scala/ActionTest.scala
@@ -1,13 +1,10 @@
 import org.scalatest.FunSuite
 import com.typesafe.scalalogging.Logger
 
-import scala.reflect.classTag
-
 import game.Logging.logger
 
 import game.core._
 import game.core.actions._
-import game.core.Card.CardId
 
 import game.example._
 import game.example.actions.attacks._
@@ -15,42 +12,8 @@ import game.example.DefaultAttributes.DefaultHand
 import game.example.DefaultAttributes.DefaultPlayers
 import game.example.actions.PriorityIncrementer
 
-final class ExampleCardWithArgs protected (val arg1: Int, val arg2: String) extends TypedCard[ExampleCardWithArgs] {
-  override def toString = s"Card(id=$id, $tag, arg1=$arg1, arg2=$arg2)"
-}
 
-class GameTest extends FunSuite {
-  
-  test("CardFactory test") { 
-    implicit val cardFactory = new DefaultCardFactory
-    
-    val c1 = Card[AttackCard]()
-    logger.info(s"$c1")
-    
-    val c5 = Card[CatCard](id = 5)()
-    logger.info(s"$c5")
-    
-    val c2 = Card[CatCard]()
-    logger.info(s"$c2")
-    
-    val c3 = Card(classTag[MassedAttackCard])()
-    logger.info(s"$c3")
-    
-    val c10 = Card(classTag[MassedAttackCard], id = 10)()
-    logger.info(s"$c10")
-    
-    val c4 = Card[ExampleCardWithArgs](100, "Hej c4")
-    logger.info(s"$c4")
-    
-    val c9 = Card[ExampleCardWithArgs](id = 9)(100, "Hej c9")
-    logger.info(s"$c9")
-    
-    val c6 = Card(classTag[ExampleCardWithArgs])(100, "Hej c6")
-    logger.info(s"$c6")
-    
-    val c20 = Card(classTag[ExampleCardWithArgs], id = 20)(100, "Hej c20")
-    logger.info(s"$c20")
-  }
+class ActionTest extends FunSuite {
   
   test("Action test") {
     implicit val cardFactory = new DefaultCardFactory
